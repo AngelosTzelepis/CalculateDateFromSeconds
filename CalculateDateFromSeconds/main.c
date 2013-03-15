7ef8a1406536a5c0d1efd5b8c3ce12a3ec208047
Angelos Tzelepis <agtzelepis@gmail.com>
2013-03-15 12:26:17 -0400

    Initial Commit

---
+++ CalculateDateFromSeconds/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  CalculateDateFromSeconds
+//
+//  Created by Angelos Tzelepis on 3/15/13.
+//  Copyright (c) 2013 Angelos Tzelepis. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
